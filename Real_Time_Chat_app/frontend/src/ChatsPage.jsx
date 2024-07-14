166cf236264eece66a5da539ae2f1a76bd6e3560
Pathum Dilshan <156510213+Pathum-SC12402@users.noreply.github.com>
2024-07-14 18:25:50 +0530

    frontend is created

--- Real_Time_Chat_app/frontend/src/ChatsPage.jsx
+++ Real_Time_Chat_app/frontend/src/ChatsPage.jsx
@@ -1,4 +1,21 @@
-const ChatsPage = () => {
-    return <div className="background">chats...</div>;
-  };
+import React from 'react';
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '0a826b7a-fe07-4eea-a24e-8a2da5bd5d16', // Replace with your actual Chat ID
+        props.user.username, // Assuming props.user.username is correctly passed
+        props.user.secret // Assuming props.user.secret is correctly passed
+    );
+
+    return (
+        <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+            <MultiChatSocket {...chatProps} />
+            <div style={{ flex: 1, overflowY: 'auto' }}>
+            <MultiChatWindow {...chatProps} />
+            </div>
+        </div>
+    );
+};
+
 export default ChatsPage;
